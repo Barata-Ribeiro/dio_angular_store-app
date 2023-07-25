b57d4b0e2afda442091dc2492100e534fcf6797b
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-07-25 17:10:16 -0300

    feat: atomized menu bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuBarItemComponent } from './components/menu-bar/menu-bar-item/menu-bar-item.component';
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
-  declarations: [AppComponent, MenuBarComponent],
+  declarations: [AppComponent, MenuBarComponent, MenuBarItemComponent],
   imports: [BrowserModule, AppRoutingModule, PagesModule],
   providers: [],
   bootstrap: [AppComponent],
