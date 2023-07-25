1b98c8abc7b6105e0ef247c884d8cf6fb06eac30
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-07-25 09:40:10 -0300

    feat: add card pricing label style and logic

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.css']
+  styleUrls: ['./card-pricing.component.css'],
 })
 export class CardPricingComponent {
+  @Input() gameType: string = '';
+  @Input() gamePrice: number = 0;
 
+  constructor() {}
+
+  convertToBRL(price: number): string {
+    const formatter = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+
+    return formatter.format(price);
+  }
 }
